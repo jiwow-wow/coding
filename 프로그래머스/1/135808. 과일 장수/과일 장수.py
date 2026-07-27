c83a68a081686cd8b5b6ed978ab58391c2658539
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-27 22:53:37 +0900

    [level 1] Title: 과일 장수, Time: 93.00 ms, Memory: 23.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    answer = 0    
+    score.sort(reverse=True)
+    
+    for i in range(0,len(score)-m+1 ,m):
+        box = score[i: i+ m]
+        answer += min(box) * m
+    
+    return answer
