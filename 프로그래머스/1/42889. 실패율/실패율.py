4706dfe7fa8bc46249188cb42938a14ce65bd8e5
Jiwon Jung <jibbang@mju.ac.kr>
2026-08-01 22:10:43 +0900

    [level 1] Title: 실패율, Time: 1058.55 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,12 @@
+
 def solution(N, stages):
-    answer = []
-    
-    clear_player = []
-    for i in range(1, N+2):
-            clear_player.append(stages.count(i))
-    
-    for s in range(1, N+1):
-        if sum(clear_player[s-1:]) != 0:
-            answer.append( clear_player[s-1] / sum(clear_player[s-1:]))
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
         else:
-            answer.append(0)
-    
-    fail = list(enumerate(answer, start =1) )
-    fail.sort(key =lambda x: x[1], reverse = True)
-    
-    
-    return [s for s,f in fail ]
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
