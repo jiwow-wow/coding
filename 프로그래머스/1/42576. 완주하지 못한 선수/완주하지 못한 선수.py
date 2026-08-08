494d6731e45b43ec04ca3a770a01eaa7f95cad5c
Jiwon Jung <jibbang@mju.ac.kr>
2026-08-08 23:33:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.48 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -8,7 +8,7 @@ def solution(participant, completion):
 
     return participant[-1]
         
-"""
+""" 카운터로 풀이
 from collections import Counter
 
 
@@ -18,5 +18,21 @@ def solution(participant, completion):
 
     # 남은 딕셔너리에서 키(이름)를 가져옵니다.
     return list(answer.keys())[0]
+
+"""
+
+""" 해시함수로 풀이
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
 
 """
