b0001642b21dfeaa016e7818dc7318d176562e20
Jiwon Jung <jibbang@mju.ac.kr>
2026-08-17 16:14:22 +0900

    [level 1] Title: 숫자 짝꿍, Time: 82.58 ms, Memory: 25.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128#) 
 
 ### 성능 요약
 
-메모리: 25.6 MB, 시간: 82.25 ms
+메모리: 25.6 MB, 시간: 82.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 17일 15:57:48
+2026년 08월 17일 16:14:20
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,9 +1,17 @@
+"""
+from collections import Counter
+
+# 단 1번만 순회하여 각 숫자의 개수를 셈 - O(N + M)
+cnt_x = Counter(X)
+cnt_y = Counter(Y)
+
+# Counter의 교집합(&) 연산: 두 객체에 모두 존재하는 요소의 최소 개수를 알아서 남김
+common = cnt_x & cnt_y
+"""
+
 def solution(X, Y):
     answer = []
     num_list = [ str(i) for i in range(9,-1,-1)]
-    
-    
-
     
     for n in num_list:
         if n in X and n in Y:
@@ -13,28 +21,8 @@ def solution(X, Y):
     
     if result == '':
         return '-1'
-    elif result[0] == '0' :
+    elif result[0] == '0':
         return '0'
     
     return result
     
-    
-    
-    
-    
-    
-#     set_x = set(X)
-#     set_y = set(Y)
-#     set_xy = set_y - set_x
-    
-#     list_answer = list(Y)
-    
-#     for num in set_xy:
-#         list_answer.remove(num)
-
-#     if len(list_answer) == 0:
-#         return '-1'
-    
-#     list_answer.sort(reverse =True)
-    
-#     return str(int(''.join(list_answer)))
